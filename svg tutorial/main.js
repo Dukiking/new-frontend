070f726bf8504ca81d0c9c2e83f94a64c161a380
Dusan <dusan.misic@students.fhnw.ch>
2019-11-27 14:10:35 +0100

    resizing the shield

--- svg tutorial/main.js
+++ svg tutorial/main.js
@@ -1,19 +1,20 @@
 let x = 100;
 
 const shieldHeight = document.getElementById("heights");
+const shieldWidth = document.getElementById("width");
 const outerRect = document.getElementById("outerRect");
+const innerRect = document.getElementById("innerRect");
 
 shieldHeight.oninput = () => {
     const height = shieldHeight.innerHTML;
     console.log(`textfield changed and height is ${height}`);
     outerRect.setAttribute("height", height);
+    innerRect.setAttribute("height", height-20);
 };
-
-const shieldWidth = document.getElementById("width");
-const outerRect = document.getElementById("outerRect");
 
 shieldWidth.oninput = () => {
     const width = shieldWidth.innerHTML;
     console.log(`textfield changed and width is ${width}`);
     outerRect.setAttribute("width", width);
+    innerRect.setAttribute("Width", width-20);
 };
